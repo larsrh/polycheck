96a62f20aec5b856fd1d0ce72e9fc1ff4b862805
Lars Hupel <lars.hupel@mytum.de>
2016-04-30 11:40:50 +0200

    implement Cogen

--- src/main/scala/types/Small.scala
+++ src/main/scala/types/Small.scala
@@ -4,18 +4,20 @@ import org.scalacheck._
 
 object small {
 
-  sealed trait One
-  sealed trait Two
+  sealed trait One { def perturb: Long }
+  sealed trait Two { def perturb: Long }
 
-  case object A extends One with Two
-  case object B extends Two
+  case object A extends One with Two { val perturb = 1L }
+  case object B extends Two          { val perturb = 2L }
 
   object One {
     implicit def oneArbitrary: Arbitrary[One] = Arbitrary(Gen.oneOf(List(A)))
+    implicit def oneCogen: Cogen[One] = Cogen(_.perturb)
   }
 
   object Two {
     implicit def twoArbitrary: Arbitrary[Two] = Arbitrary(Gen.oneOf(List(A, B)))
+    implicit def twoCogen: Cogen[Two] = Cogen(_.perturb)
   }
 
 }
